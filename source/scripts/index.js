541fd08e8876556bb2a1ae9f949fbda6f86c39be
Mishok1 <mm21112000@gmail.com>
2024-03-08 17:25:16 +0300

    Fixs select lists

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,4 +3,23 @@ const select = document.querySelector('.select');
 
 button.addEventListener('click', () => {
   select.classList.toggle('select--opened');
+  button.classList.toggle('button--opened');
 });
+
+const buttonBeds = document.querySelector('.button-beds');
+const selectBeds = document.querySelector('.select-beds');
+
+buttonBeds.addEventListener('click', () => {
+  selectBeds.classList.toggle('select--opened');
+  buttonBeds.classList.toggle('button--opened');
+});
+
+const buttonBaths = document.querySelector('.button-baths');
+const selectBaths = document.querySelector('.select-baths');
+
+buttonBaths.addEventListener('click', () => {
+  selectBaths.classList.toggle('select--opened');
+  buttonBaths.classList.toggle('button--opened');
+});
+
+
